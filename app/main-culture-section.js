f385cd8a98f36b7a719935dbbcda1a23977cb5b3
Krishnan <krishnan.narayanan15@gmail.com>
2024-09-09 19:14:14 +0530

    Adding the temple name

--- app/main-culture-section.js
+++ app/main-culture-section.js
@@ -8,25 +8,27 @@ export default function MainCultureSection() {
           </h1>
           <p>
             In Rajapalayam, within a small town called
-            Alangulam, there is a temple where the concept
-            of "kuladeivam" plays a significant role. In
-            southern India, a kuladeivam refers to a deity
-            associated with a particular family or caste,
-            though many families may share the same deity.
-            One such deity is Ayyanar Swamy, worshipped by
-            families from 34 nearby villages. A unique
-            belief surrounds Bhairava, a dog, who serves as
-            the guardian of Ayyanar, the main deity. Before
-            approaching Ayyanar in the temple's sanctum,
-            devotees must first seek Bhairava's permission,
-            highlighting the deep reverence for the dog.
-            This respect extends to daily customs, where
-            households offer food first to a crow, then to a
-            dog, before they themselves eat. This tradition
-            symbolizes the strong cultural connection to the
-            native breed of dogs and reflects how, even
-            today, certain cultures in southern Tamil Nadu
-            continue to revere the dog as a divine being.
+            Alangulam, stands the Shree Arulmigu
+            Thalakkudaiyaar Ayyanaar Swamy Temple, where the
+            concept of "kuladeivam" plays a significant
+            role. In southern India, a kuladeivam refers to
+            a deity associated with a particular family or
+            caste, though many families may share the same
+            deity. One such deity is Ayyanar Swamy,
+            worshipped by families from 34 nearby villages.
+            A unique belief surrounds Bhairava, a dog, who
+            serves as the guardian of Ayyanar, the main
+            deity. Before approaching Ayyanar in the
+            temple's sanctum, devotees must first seek
+            Bhairava's permission, highlighting the deep
+            reverence for the dog. This respect extends to
+            daily customs, where households offer food first
+            to a crow, then to a dog, before they themselves
+            eat. This tradition symbolizes the strong
+            cultural connection to the native breed of dogs
+            and reflects how, even today, certain cultures
+            in southern Tamil Nadu continue to revere the
+            dog as a divine being.
           </p>
         </div>
         <div className="my-8 lg:col-span-2 flex justify-center">
